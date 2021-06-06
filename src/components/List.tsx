47cf139ec663a1ffceecb4385215dcbb4a484da8
Inga Michneviciute <ingeramich@gmail.com>
2021-06-06 09:53:06 +0200

    initial setup

---
+++ src/components/List.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card";
+
+const List: React.FC = () => {
+    return (
+        <div className="image-list">
+            List
+            <Card />
+        </div>
+    );
+};
+
+export default List;
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import List from "../components/List";
+
+const Homepage: React.FC = () => {
+    return (
+        <div className="homepage">
+            Homepage
+            <List />
+        </div>
+    );
+};
+
+export default Homepage;
